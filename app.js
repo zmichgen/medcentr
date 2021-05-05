5150f7c20292a65a1d73b2b131d34863f8c5184d
M.Zyranov <zmichgen@gmail.com>
2021-05-05 22:41:16 +0300

    fix settings

--- app.js
+++ app.js
@@ -7,16 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var resultsRouter = require('./routes/results');
 
-const livereload = require("livereload");
+const livereload = require('livereload');
 
 const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
+liveReloadServer.server.once('connection', () => {
+    setTimeout(() => {
+        liveReloadServer.refresh('/');
+    }, 100);
 });
-const connectLivereload = require("connect-livereload");
-
+const connectLivereload = require('connect-livereload');
 
 liveReloadServer.watch(path.join(__dirname, 'public'));
 
@@ -37,19 +36,19 @@ app.use('/', indexRouter);
 app.use('/results', resultsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,38 @@
-const express = require("express");
+const express = require('express');
 var router = express.Router();
 
 // импорт коннектора к базе данных
 const pool = require('../db/db');
 
 // sql запрос - справочник видов услуг
-const getServiceTypes = `select * from service_types`
-const getServiceList = `select * from services`
+const getServiceTypes = `select * from service_types`;
+const getServiceList = `select * from services`;
 
 // функция получения справочников из БД возвращает JSON со справочниками
 async function getJson() {
+    // получение справочника видов услуг из БД
+    let req = await pool.execute(getServiceTypes);
+    const service_types = req[0];
+    //----------------------------
+    req = await pool.execute(getServiceList);
+    const services = req[0];
 
-  // получение справочника видов услуг из БД
-  let req = await pool.execute(getServiceTypes)
-  const service_types = req[0]
-  //----------------------------
-  req = await pool.execute(getServiceList)
-  const services = req[0];
-
-  // возвращаем справочники в виде полей объекта (JSON)
-  return {service_types, services}
+    // возвращаем справочники в виде полей объекта (JSON)
+    return { service_types, services };
 }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-// получаем json со справочниками
-  getJson()
-  .then(json => {
-    console.log(json)
-    /**
-     * отдаем справочники на темплейт index.hbs ,
-     * рендерим страницу index.html,
-     * отправляем index.html в браузер
-     */
-    res.render('index', json);
-  })
-
+router.get('/', function (req, res, next) {
+    // получаем json со справочниками
+    getJson().then((json) => {
+        console.log(json);
+        /**
+         * отдаем справочники на темплейт index.hbs ,
+         * рендерим страницу index.html,
+         * отправляем index.html в браузер
+         */
+        res.render('index', json);
+    });
 });
 
 module.exports = router;
