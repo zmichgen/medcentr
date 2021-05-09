9e72207040440751f6d5dd03e6ce9d2aa477bea0
M.Zyranov <zmichgen@gmail.com>
2021-05-09 18:15:24 +0300

    fix

--- routes/results.js
+++ routes/results.js
@@ -6,20 +6,20 @@ let result = [];
 
 /**
  * функция создающая SQL запрос в зависимости от данных, пришедших из формы
- * @param {*} data - объект поля которого содержат данные из инпутов формы (поле name в инпуте)
+ * @param {*} data - объект поля которого содержат данные из инпутов формы
  * @returns SQL запрос
  */
 function createSQL(data) {
     let where = '';
+
     // вид услуг
     const { service_type, name, service_id } = data;
+    console.log(data);
+    const searchServices = !Number.isNaN(+service_id)
+        ? `and services2.id = ${service_id}`
+        : '';
 
-    if (name) {
-        where = !where
-            ? `where centers.name like '%${name}%'`
-            : `${where} AND centers.name like '%${name}%'`;
-    }
-    if (service_type) {
+    if (!Number.isNaN(+service_type)) {
         // если есть вид услуг - выбираем только с тем видом, который указан
         where = !where
             ? `where centers.service_type_id = ${service_type}`
@@ -31,7 +31,7 @@ function createSQL(data) {
     join centers_services centers_services1 on centers.id = centers_services1.center_id
     join services services1 on services1.id = centers_services1.service_id
     join centers_services centers_services2 on centers_services2.center_id = centers.id
-    join services services2 on services2.id = centers_services2.service_id and services2.id = ${service_id}
+    join services services2 on services2.id = centers_services2.service_id ${searchServices}
     ${where}
     group by centers.id;`;
     return result;
@@ -41,15 +41,16 @@ function createSQL(data) {
 router.post('/', function (req, response, next) {
     // получаем тело запроса (данные из формы)
     const sql = createSQL(req.body);
+    // console.log(sql); // sql запрос
     // выполняем SQL запрос
     pool.execute(sql)
         .then((res) => {
             // получаем данные из БД
             result = res[0].map((i) => ({
                 ...i,
-                serviceList: i.serviceList.split(','),
+                serviceList: Array.from(new Set(i.serviceList.split(','))), // массив уникальных записей из поля serviceList
             }));
-            console.log(result);
+            // console.log(result); // результат sql запроса и его преобразования
             // рендерим страницу results.html с данными из БД и отправляем в браузер
             response.render('results', { result });
         })
